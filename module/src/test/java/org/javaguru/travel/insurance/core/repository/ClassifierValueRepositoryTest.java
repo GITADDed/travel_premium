ef2bb80065ef34b1829cabc111337c0b1ae41de2
Sergei Makarov <kahu908@gmail.com>
2026-03-13 03:21:17 +0300

    add liquibase migration

--- module/src/test/java/org/javaguru/travel/insurance/core/repository/ClassifierValueRepositoryTest.java
+++ module/src/test/java/org/javaguru/travel/insurance/core/repository/ClassifierValueRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
@@ -14,6 +15,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 @DataJpaTest
 class ClassifierValueRepositoryTest {
 
